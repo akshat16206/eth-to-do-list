8d070e8813226c56ab00e110355376b41ab580b9
AKSHAT GUPTA <akshat8036@gmail.com>
2025-05-14 01:58:00 +0530

    Implement Todo List smart contract and frontend integration
    - Added a smart contract for a Todo List with task creation functionality.
    - Created a basic HTML structure for the frontend with Bootstrap styling.
    - Implemented Web3.js integration to connect the frontend with the Ethereum blockchain.
    - Set up a basic configuration for BrowserSync to serve the application.

--- contracts/todolist.sol
+++ contracts/todolist.sol
@@ -3,4 +3,22 @@ pragma solidity ^0.8.0;
 
 contract todolist {
    uint public taskcount = 0;
+
+   struct Task {
+      uint id;
+      string content;
+      bool completed;
+
+   } 
+   mapping(uint=> Task) public tasks;
+
+   constructor() public{
+      createtask("Check out out website");
+   }
+
+   function  createtask(string memory _content) public {
+   taskcount ++;
+   tasks[taskcount] = Task(taskcount,_content, false);
+
+   }
 }
